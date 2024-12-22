5141e17688afb3830dcc0964cc3078a70fd21d08
Afonso Raposo <afonsocraposo@gmail.com>
2024-12-22 17:06:05 +0000

    feat: day 22

--- internal/day22/day22.go
+++ internal/day22/day22.go
@@ -3,6 +3,7 @@ package day22
 import (
 	"fmt"
 	"log"
+	"slices"
 	"strconv"
 
 	"github.com/afonsocraposo/advent-of-code-2024/internal/utils/filereader"
@@ -10,7 +11,7 @@ import (
 
 const day = 22
 
-var examples = []int{1}
+var examples = []int{1, 2}
 
 func Main() {
 	log.Printf("DAY %d\n", day)
@@ -31,23 +32,23 @@ func Main() {
 			}
 			expectedSolution, err := filereader.ReadDayExampleSolution(day, example, part)
 			if err != nil {
-				log.Fatalln(err)
+				continue
 			}
 
 			exampleSolution := partMethod(exampleLines)
 			if exampleSolution != expectedSolution {
-				log.Fatalf("WRONG solution for example %d. Expected: %s, Actual: %s\n", example, expectedSolution, exampleSolution)
+				log.Printf("WRONG solution for example %d. Expected: %s, Actual: %s\n", example, expectedSolution, exampleSolution)
 			} else {
 				log.Printf("The solution is CORRECT for example %d. Expected/actual: %s\n", example, exampleSolution)
 			}
 
-			inputLines, err := filereader.ReadDayInput(day, 1)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			inputSolution := partMethod(inputLines)
-            log.Printf("The solution for the input is: %s\n", inputSolution)
 		}
+		inputLines, err := filereader.ReadDayInput(day, 1)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		inputSolution := partMethod(inputLines)
+		log.Printf("The solution for the input is: %s\n", inputSolution)
 	}
 }
 
@@ -88,9 +89,56 @@ func part1(lines []string) string {
 	return fmt.Sprintf("%d", solution)
 }
 
+func hash(pattern []int) string {
+	return fmt.Sprintf("%d,%d,%d,%d", pattern[0], pattern[1], pattern[2], pattern[3])
+}
+
 func part2(lines []string) string {
-	fmt.Println(lines)
+	buyersPatterns := []map[string]int{}
+	uniquePatterns := []string{}
+	for _, line := range lines {
+		start, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		patterns := map[string]int{}
+		buffer := []int{}
+		prev := start
+		for range 2000 {
+			dPrev := prev % 10
+			n := next(prev)
+			d := n % 10
+
+			diff := d - dPrev
+			buffer = append(buffer, diff)
+
+			if len(buffer) >= 4 {
+				h := hash(buffer)
+				_, found := patterns[h]
+				if !found {
+					patterns[h] = d
+					uniquePatterns = append(uniquePatterns, h)
+				}
+				buffer = buffer[1:]
+			}
+
+			prev = n
+		}
+		buyersPatterns = append(buyersPatterns, patterns)
+	}
+
+	slices.Sort(uniquePatterns)
+	uniquePatterns = slices.Compact(uniquePatterns)
 
 	solution := 0
+	for _, pattern := range uniquePatterns {
+		score := 0
+		for _, buyersPattern := range buyersPatterns {
+			score += buyersPattern[pattern]
+		}
+		if score > solution {
+			solution = score
+		}
+	}
 	return fmt.Sprintf("%d", solution)
 }
